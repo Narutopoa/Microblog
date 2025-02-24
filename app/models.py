075a56561bc0da873220cc19d13f028359c2121c
mikeo <mikeoluwole@gmail.com>
2025-02-24 02:15:31 -0600

    Login, registration, user session handling, logout

--- app/models.py
+++ app/models.py
@@ -1,10 +1,18 @@
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from app import login
+from flask_login import UserMixin
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
     __tablename__ = 'user' # set table name, user, by default
     
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -20,6 +28,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
         
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
